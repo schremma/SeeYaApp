05de9ca2ab398d04ea2791a366d05992bfbcc974
schremma <schremma@gmail.com>
2016-04-13 12:41:27 +0200

    Added MainActivity from Laura and Isabella's project on drive

--- app/src/main/java/com/group16/seeyaapp/main/MainActivity.java
+++ app/src/main/java/com/group16/seeyaapp/main/MainActivity.java
@@ -1,9 +1,13 @@
 package com.group16.seeyaapp.main;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.group16.seeyaapp.R;
+import com.group16.seeyaapp.login.TestLoginActivity;
+import com.group16.seeyaapp.register.TestRegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.signIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(v);
+            }
+        });
+
+        findViewById(R.id.signUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp(v);
+            }
+        });
+    }
+
+    public void signIn(View view) {
+        startActivity(new Intent(MainActivity.this, TestLoginActivity.class));
+    }
+    public void signUp(View view){
+        Intent intent = new Intent(this, TestRegisterActivity.class);
+        startActivity(intent);
     }
 }
